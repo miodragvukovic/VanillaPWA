d103a816c11bf8c41ddb7a7f54d693f394f650be
miodrag.vukovic <35942671+miodragvukovic@users.noreply.github.com>
2019-05-26 00:39:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 for ( var trigger of document.getElementsByClassName('trigger') ) {
 	trigger.addEventListener('click', function(){
+		let selected = this.getAttribute('data-target')
 		if ( this.classList.contains('active') ) {
 			return false
 		} else {
@@ -7,10 +8,10 @@ for ( var trigger of document.getElementsByClassName('trigger') ) {
 				trigger1.classList.remove('active')
 			}
 			this.classList.add('active')
+			console.log(this.getAttribute('data-target'))
 		}
 		var string = ''
 		var el = document.getElementsByClassName('section')[0]
-		let selected = this.getAttribute('data-target')
 		string += "<div class='section "+selected+"'>"
 		string += "</div>"
 		document.querySelector('#main').insertAdjacentHTML('beforeend', string)
@@ -23,23 +24,18 @@ for ( var trigger of document.getElementsByClassName('trigger') ) {
 	})
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function request(page) {
+	const request = new XMLHttpRequest()
+	var url = "https://cdn.jsdelivr.net/gh/miodragvukovic/VanillaPWA/"+page+".html"
+	request.open('get', url, true)
+	request.onreadystatechange = function() {
+		if ( request.readyState == 4 && request.status == 200 ) {
+			var data = request.responseText
+			console.log(data)
+		}
+	}
+	request.send()
+}
 
 
 
